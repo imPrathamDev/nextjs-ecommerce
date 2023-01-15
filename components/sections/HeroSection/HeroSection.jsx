acee82947e7ee6e5aadd8e82fad85a948e6e2650
Pratham <pratham.sharma2105@gmail.com>
2023-01-15 22:17:23 +0530

    added left margin in content

--- components/sections/HeroSection/HeroSection.jsx
+++ components/sections/HeroSection/HeroSection.jsx
@@ -16,7 +16,7 @@ function HeroSection({ image, title, secondaryTitle, desc, url, buttonTitle }) {
       />
       <div className="block inset-0 absolute bg-gradient-to-r from-primary-white to-transparent w-full h-full"></div>
       <div className="relative max-w-screen-xl px-4 py-24 lg:h-1/2 lg:items-center lg:flex mx-6">
-        <div className="max-w-xl text-center sm:text-left">
+        <div className="max-w-xl text-center sm:text-left ml-10">
           <motion.h1 className="text-3xl font-extrabold sm:text-5xl">
             {title}
             {secondaryTitle && (
