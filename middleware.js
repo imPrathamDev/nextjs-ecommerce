152fd3531c78136e50645696629b347e8acca6af
Pratham <pratham.sharma2105@gmail.com>
2023-01-25 22:39:51 +0530

    added new secure routes

--- middleware.js
+++ middleware.js
@@ -4,12 +4,32 @@ import { getToken } from "next-auth/jwt";
 const secret = process.env.JWT_SECRET;
 
 export async function middleware(req) {
+  if (
+    req.nextUrl.pathname.startsWith("/account") ||
+    req.nextUrl.pathname.startsWith("/checkout")
+  ) {
+    const token = await getToken({ req, secret });
+    if (token) {
+      return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
   if (req.nextUrl.pathname.startsWith("/adminDashboard")) {
     const token = await getToken({ req, secret });
     if (token?.type === "admin") {
       return NextResponse.next();
     } else if (token?.type === "user") {
       return NextResponse.redirect(new URL("/account", req.url));
+    }
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (req.nextUrl.pathname.startsWith("/api/test")) {
+    const token = await getToken({ req, secret });
+    console.log("Token =>", token);
+    if (!token) {
+      return NextResponse.next();
     }
     return NextResponse.redirect(new URL("/", req.url));
   }
