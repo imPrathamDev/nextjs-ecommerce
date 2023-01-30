040fcdba42cab3e572eb240986699df116ff0cab
Pratham <pratham.sharma2105@gmail.com>
2023-01-30 14:29:21 +0530

    improve the api

--- pages/api/wishlist/checkWishlist.js
+++ pages/api/wishlist/checkWishlist.js
@@ -1,19 +1,32 @@
 import connectdb from "../../../utils/connectMongo";
 import Wishlist from "../../../models/Wishlist";
+import { getToken } from "next-auth/jwt";
 
-export default async function handler(req, res){
-    const { userId, productId } = req.body;
-    console.log('userId =>', userId)
-    console.log('productId =>', productId)
-    if(req.method === 'POST'){
+const secret = process.env.JWT_SECRET;
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const token = await getToken({ req, secret });
+    if (token) {
+      const { productId } = req.body;
+      if (productId) {
         await connectdb();
-        const isWishlist = await Wishlist.countDocuments({ userId, productId });
-        if(isWishlist > 0){
-            res.json({ sucess: true })
+        const isWishlist = await Wishlist.countDocuments({
+          userId: token._id,
+          productId,
+        });
+        if (isWishlist > 0) {
+          res.json({ success: true });
         } else {
-            res.json({ sucess: false })
+          res.json({ success: false });
         }
+      } else {
+        res.json({ success: false });
+      }
     } else {
-        res.json({ sucess: false })
+      res.json({ success: false });
     }
+  } else {
+    res.json({ success: false });
+  }
 }
