a0e2d64851fc730ed1fbd10c5d0c2eac6fcafba5
Pratham <pratham.sharma2105@gmail.com>
2023-01-30 14:25:41 +0530

    improve api

--- pages/api/products/getProducts/index.js
+++ pages/api/products/getProducts/index.js
@@ -1,30 +1,47 @@
 import Product from "../../../../models/Products";
 import connectdb from "../../../../utils/connectMongo";
 
+const DEFAULT_LIMIT = 8;
+
 export default async function handler(req, res) {
   try {
     await connectdb();
-    if (req.query.category) {
+    const { category, sort, limit } = req.query;
+    if (category) {
       let products;
-      products = await Product.find({ category: req.query.category });
+      products = await Product.find({ category }).limit(
+        limit ? limit : DEFAULT_LIMIT
+      );
       if (products.length <= 1) {
-        products = await Product.find().sort({ createdAt: -1 }).limit(8);
-        res
-          .status(200)
-          .json({ success: true, length: products.length, products });
+        products = await Product.find()
+          .sort({ createdAt: -1 })
+          .limit(limit ? limit : DEFAULT_LIMIT);
+        res.status(200).json({
+          success: true,
+          length: products.length,
+          products,
+          testCode: 1,
+        });
       }
-      res
-        .status(200)
-        .json({ success: true, length: products.length, products });
-    } else if (req.query.sort) {
+      res.status(200).json({
+        success: true,
+        length: products.length,
+        products,
+        testCode: 2,
+      });
+    } else if (sort) {
       if (req.query.sort === "new") {
-        const products = await Product.find().sort({ createdAt: -1 });
+        const products = await Product.find()
+          .sort({ createdAt: -1 })
+          .limit(limit ? limit : DEFAULT_LIMIT);
         res
           .status(200)
           .json({ success: true, length: products.length, products });
       }
     } else {
-      const products = await Product.find();
+      const products = await Product.find().limit(
+        limit ? limit : DEFAULT_LIMIT
+      );
       res
         .status(200)
         .json({ success: true, length: products.length, products });
--- pages/api/products/getProductsByIds/index.js
+++ pages/api/products/getProductsByIds/index.js
@@ -1,24 +1,23 @@
 import Product from "../../../../models/Products";
 import connectdb from "../../../../utils/connectMongo";
 
-export default async function handler(req, res){
+export default async function handler(req, res) {
+  if (req.body.ids) {
     const idsList = req.body.ids;
-  try {
-      console.log('CONNECTING TO DB');
+    try {
       await connectdb();
-      console.log('CONNECTED TO DB');
-
-      console.log('FETCHING DATA FROM DB');
       const products = await Product.find({
-        '_id': {
-            $in: idsList,
-        }
+        _id: {
+          $in: idsList,
+        },
       });
-      console.log('FETCHED DATA FROM DB');
 
-      res.status(202).json({ success: true, length: products.length, products });
-
-  } catch (err) {
-    res.json({ success: false, error: err })
+      return res
+        .status(200)
+        .json({ success: true, length: products.length, products });
+    } catch (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
   }
+  return res.status(400).json({ success: false, error: "Empty IDs" });
 }
