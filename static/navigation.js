afcb095dbd60c29d831043c7235d6c365f22d594
Pratham <pratham.sharma2105@gmail.com>
2023-01-30 14:27:51 +0530

    created new files

---
+++ static/navigation.js
@@ -0,0 +1,81 @@
+export const navigation = {
+  categories: [
+    {
+      id: "collections",
+      name: "collections",
+      featured: [
+        {
+          name: "New Arrivals",
+          href: "#",
+          imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg",
+          imageAlt:
+            "Models sitting back to back, wearing Basic Tee in black and bone.",
+        },
+        {
+          name: "Basic Tees",
+          href: "#",
+          imageSrc:
+            "https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg",
+          imageAlt:
+            "Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.",
+        },
+      ],
+      sections: [
+        {
+          id: "categories",
+          name: "Categories",
+          items: [
+            { name: "Tops", href: "#" },
+            { name: "Dresses", href: "#" },
+            { name: "Pants", href: "#" },
+            { name: "Denim", href: "#" },
+            { name: "Sweaters", href: "#" },
+            { name: "T-Shirts", href: "#" },
+            { name: "Jackets", href: "#" },
+            { name: "Activewear", href: "#" },
+            { name: "Browse All", href: "#" },
+          ],
+        },
+        {
+          id: "colours",
+          name: "Colours",
+          items: [
+            { name: "Watches", href: "#" },
+            { name: "Wallets", href: "#" },
+            { name: "Bags", href: "#" },
+            { name: "Sunglasses", href: "#" },
+            { name: "Hats", href: "#" },
+            { name: "Belts", href: "#" },
+          ],
+        },
+        {
+          id: "style",
+          name: "Style",
+          items: [
+            { name: "Full Nelson", href: "#" },
+            { name: "My Way", href: "#" },
+            { name: "Re-Arranged", href: "#" },
+            { name: "Counterfeit", href: "#" },
+            { name: "Significant Other", href: "#" },
+          ],
+        },
+        {
+          id: "stones",
+          name: "Stones",
+          items: [
+            { name: "Full Nelson", href: "#" },
+            { name: "My Way", href: "#" },
+            { name: "Re-Arranged", href: "#" },
+            { name: "Counterfeit", href: "#" },
+            { name: "Significant Other", href: "#" },
+          ],
+        },
+      ],
+    },
+  ],
+  pages: [
+    { name: "About Us", href: "#" },
+    { name: "Contact", href: "#" },
+  ],
+};
---
+++ static/staticData.js
@@ -0,0 +1,53 @@
+export const Countries = [
+  {
+    name: "India",
+  },
+];
+
+export const DEFAULT_SHIPPING_PRICE = 0;
+export const heroBanners = [
+  {
+    title: "Traditional Jewellery",
+    secondaryTitle: "On Sriya.",
+    url: "http://localhost:3000/",
+    image: {
+      url: "https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(1).png?alt=media&token=8963b61f-bf65-4c95-b5ef-f9be13584397",
+      alt: "Sriya",
+    },
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, reprehenderit veniam molestias laudantium quasi sed neque laboriosam autem.",
+    buttonTitle: "Shop Now",
+  },
+  {
+    title: "New Collections",
+    secondaryTitle: "On Sriya.",
+    url: "http://localhost:3000/",
+    image: {
+      url: "https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(2).png?alt=media&token=23fa4b19-2144-4171-9b8a-dc696ccf2327",
+      alt: "Sriya",
+    },
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, reprehenderit veniam molestias laudantium quasi sed neque laboriosam autem.",
+    buttonTitle: "Shop Now",
+  },
+  {
+    title: "Shop Rings",
+    secondaryTitle: "On Sriya.",
+    url: "http://localhost:3000/",
+    image: {
+      url: "https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(3).png?alt=media&token=96fdfc5b-9ae0-4800-a890-ec8e0ef241d7",
+      alt: "Sriya",
+    },
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, reprehenderit veniam molestias laudantium quasi sed neque laboriosam autem.",
+    buttonTitle: "Shop Rings",
+  },
+  {
+    title: "Indian Collections",
+    secondaryTitle: "On Sriya.",
+    url: "http://localhost:3000/",
+    image: {
+      url: "https://firebasestorage.googleapis.com/v0/b/replay-chat-dd920.appspot.com/o/j-images%2Fbanner%20(4).png?alt=media&token=9e5e97cd-d2eb-4aa4-8f4e-91027014b904",
+      alt: "Sriya",
+    },
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, reprehenderit veniam molestias laudantium quasi sed neque laboriosam autem.",
+    buttonTitle: "Shop Now",
+  },
+];
