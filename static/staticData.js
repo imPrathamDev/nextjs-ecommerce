6c477beea77795fd4b92eae63a04e3f5307a11b0
Pratham <pratham.sharma2105@gmail.com>
2023-01-31 20:25:17 +0530

    data added

--- static/staticData.js
+++ static/staticData.js
@@ -53,3 +53,48 @@ export const heroBanners = [
 ];
 
 export const DEFAULT_LIMIT = 8;
+
+export const allTestimonials = [
+  {
+    message:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet, molestiae aliquam incidunt suscipit recusandae labore ratione doloremque, architecto et illo minus quo tenetur ducimus, voluptatibus repellendus fuga aperiam vel ab! Ipsam corrupti blanditiis dolorum! Officia assumenda rem nam, eveniet enim ad inventore laudantium est illum voluptatem quis.",
+    img: "https://api.dicebear.com/5.x/lorelei/svg?seed=Pratham",
+    name: "Jeff Wezos",
+    position: "CEO of Shipping Company",
+  },
+  {
+    message:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore vel quo deserunt quos expedita minima incidunt sed tempora, a architecto consectetur reprehenderit, in repellat voluptatum.",
+    img: "https://api.dicebear.com/5.x/lorelei/svg?seed=Pratham",
+    name: "Jeff Wezos",
+    position: "CEO of Shipping Company",
+  },
+  {
+    message:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio beatae incidunt perferendis soluta facilis voluptas dicta repudiandae quasi asperiores libero, exercitationem molestiae autem sapiente dolore nulla non consequatur. Eaque, dolores.",
+    img: "https://api.dicebear.com/5.x/lorelei/svg?seed=Pratham",
+    name: "Jeff Wezos",
+    position: "CEO of Shipping Company",
+  },
+  {
+    message:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus doloribus eius aut unde, dolores accusantium!",
+    img: "https://api.dicebear.com/5.x/lorelei/svg?seed=Pratham",
+    name: "Jeff Wezos",
+    position: "CEO of Shipping Company",
+  },
+  {
+    message:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi a voluptatum quidem nulla quisquam natus velit provident earum esse, odio numquam labore recusandae similique sunt.",
+    img: "https://api.dicebear.com/5.x/lorelei/svg?seed=Pratham",
+    name: "Jeff Wezos",
+    position: "CEO of Shipping Company",
+  },
+  {
+    message:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius ut necessitatibus, repudiandae qui dolor minima.",
+    img: "https://api.dicebear.com/5.x/lorelei/svg?seed=Pratham",
+    name: "Jeff Wezos",
+    position: "CEO of Shipping Company",
+  },
+];
