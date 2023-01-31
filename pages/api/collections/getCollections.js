3b73050b09723d8d722c50549188e837c41f49b4
Pratham <pratham.sharma2105@gmail.com>
2023-01-31 11:57:57 +0530

    fix get collections api

--- pages/api/collections/getCollections.js
+++ pages/api/collections/getCollections.js
@@ -2,7 +2,7 @@ import { getCollections } from "../../../dbOperations/collectionOperations";
 
 export default async function handler(req, res) {
   try {
-    return res.status(200).json(await getCollections);
+    return res.status(200).json(await getCollections());
   } catch (error) {
     return res.status(400).json({ success: false, error: error.message });
   }
