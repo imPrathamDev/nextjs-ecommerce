5bd68de2cda0880e60b2d0c9afe6014d16e8e6df
Pratham <pratham.sharma2105@gmail.com>
2023-01-31 20:25:38 +0530

    fix images and transfer data to static

--- components/sections/Testimonials.jsx
+++ components/sections/Testimonials.jsx
@@ -1,49 +1,5 @@
 import React from "react";
-
-const allTestimonials = [
-  {
-    message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet, molestiae aliquam incidunt suscipit recusandae labore ratione doloremque, architecto et illo minus quo tenetur ducimus, voluptatibus repellendus fuga aperiam vel ab! Ipsam corrupti blanditiis dolorum! Officia assumenda rem nam, eveniet enim ad inventore laudantium est illum voluptatem quis.",
-    img: "https://www.hyperui.dev/photos/man-5.jpeg",
-    name: "Jeff Wezos",
-    position: "CEO of Shipping Company",
-  },
-  {
-    message:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore vel quo deserunt quos expedita minima incidunt sed tempora, a architecto consectetur reprehenderit, in repellat voluptatum.",
-    img: "https://www.hyperui.dev/photos/man-5.jpeg",
-    name: "Jeff Wezos",
-    position: "CEO of Shipping Company",
-  },
-  {
-    message:
-      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio beatae incidunt perferendis soluta facilis voluptas dicta repudiandae quasi asperiores libero, exercitationem molestiae autem sapiente dolore nulla non consequatur. Eaque, dolores.",
-    img: "https://www.hyperui.dev/photos/man-5.jpeg",
-    name: "Jeff Wezos",
-    position: "CEO of Shipping Company",
-  },
-  {
-    message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus doloribus eius aut unde, dolores accusantium!",
-    img: "https://www.hyperui.dev/photos/man-5.jpeg",
-    name: "Jeff Wezos",
-    position: "CEO of Shipping Company",
-  },
-  {
-    message:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi a voluptatum quidem nulla quisquam natus velit provident earum esse, odio numquam labore recusandae similique sunt.",
-    img: "https://www.hyperui.dev/photos/man-5.jpeg",
-    name: "Jeff Wezos",
-    position: "CEO of Shipping Company",
-  },
-  {
-    message:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius ut necessitatibus, repudiandae qui dolor minima.",
-    img: "https://www.hyperui.dev/photos/man-5.jpeg",
-    name: "Jeff Wezos",
-    position: "CEO of Shipping Company",
-  },
-];
+import { allTestimonials } from "../../static/staticData";
 
 function Testimonials() {
   return (
