5b63c792fe5b2fc5eb6380e1bed927610a7d6c1e
Pratham <pratham.sharma2105@gmail.com>
2023-02-01 18:40:10 +0530

    fix depcreated issue

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,12 @@
 import "../styles/globals.css";
 import "../styles/nprogress.css";
 import "../styles/slider.css";
-import "react-toastify/dist/ReactToastify.css";
 import Router from "next/router";
 import nProgress from "nprogress";
 import Context from "../context/Context";
 import { Fragment } from "react";
 import { SessionProvider } from "next-auth/react";
 import { AnimatePresence } from "framer-motion";
-import Script from "next/script";
 
 Router.events.on("routeChangeStart", nProgress.start);
 Router.events.on("routeChangeError", nProgress.done);
@@ -17,12 +15,7 @@ Router.events.on("routeChangeComplete", nProgress.done);
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <Fragment>
-      <Script
-        type="text/javascript"
-        async
-        src="https://tenor.com/embed.js"
-      ></Script>
-      <AnimatePresence exitBeforeEnter>
+      <AnimatePresence>
         <SessionProvider session={session}>
           <Context>
             <Component {...pageProps} />
